33ec2b18d13bf5f873b75d911a19f361691b0abb
Abdenasser <nasser.elidrissi065@gmail.com>
2021-08-18 12:31:56 +0100

    lets start by adding some mock data

---
+++ data.tsx
@@ -0,0 +1,44 @@
+export const data = {
+  countries: [
+    { name: "China", image: "https://i.imgur.com/1UHjH6R.jpeg" },
+    { name: "Turkey", image: "https://i.imgur.com/w0rW9Kq.jpeg" },
+    { name: "Italy", image: "https://i.imgur.com/qYftZWM.jpeg" },
+    { name: "West", image: "https://i.imgur.com/Bpwwro9.jpeg" },
+    { name: "Morocco", image: "https://i.imgur.com/E1Igsed.jpeg" },
+    { name: "Egypt", image: "https://i.imgur.com/Pslblsn.jpeg" },
+    { name: "London", image: "https://i.imgur.com/u54pnCh.jpeg" },
+  ],
+  articles: [
+    {
+      cover: "https://i.imgur.com/1UHjH6R.jpeg",
+      title: "Come visit China",
+      body: "If you're planning a trip to China, here's what you'll need to know and ...",
+      author: {
+        fullName: "Anna Livy",
+        imageUri: "https://randomuser.me/api/portraits/women/60.jpg",
+      },
+    },
+    {
+      cover: "https://i.imgur.com/ouQ6Qj5.jpeg",
+      title: "Travel to Africa",
+      body: "Explore Africa holidays and discover the best time and places to visit ...",
+      author: {
+        fullName: "Sunjay Patel",
+        imageUri: "https://randomuser.me/api/portraits/men/5.jpg",
+      },
+    },
+    {
+      cover: "https://i.imgur.com/w0rW9Kq.jpeg",
+      title: "Go Türkiye",
+      body: "So many places to see in Türkiye! Traveling in these magical lands, you will wish ... ",
+      author: {
+        fullName: "Ahmet Arif",
+        imageUri: "https://randomuser.me/api/portraits/men/31.jpg",
+      },
+    },
+  ],
+  user: {
+    fullName: "Mike Asel",
+    imageUri: "https://randomuser.me/api/portraits/men/41.jpg",
+  },
+};
